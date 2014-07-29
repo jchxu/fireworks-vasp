11a4bbbbf192b0442b8ee521900b32c93d31caa6
__sp__ <shyuep@gmail.com>
2014-07-28 17:36:16 -0700

    Inc version.

--- fireworks_vasp/__init__.py
+++ fireworks_vasp/__init__.py
@@ -1,6 +1,6 @@
 __author__ = 'Shyue Ping Ong'
 __copyright__ = 'Copyright 2013, The Materials Project'
-__version__ = '0.1'
+__version__ = '0.2'
 __maintainer__ = 'Shyue Ping Ong'
 __email__ = 'ongsp@ucsd.edu'
 __date__ = '1/31/14'
